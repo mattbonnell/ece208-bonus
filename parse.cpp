a0734597f4e1f9273ac93351a7769593a11ad621
Matt Bonnell <mdcbonne@edu.uwaterloo.ca>
2019-07-10 01:24:36 -0400

    bug fix

--- parse.cpp
+++ parse.cpp
@@ -23,6 +23,10 @@ struct formula * parse(std::string formula) {
     int orIndex = -1;
     bool isLiteral = false;
 
+    if (formula.front() == '(' && formula.back() == ')') {
+        return parse(formula.substr(1, formula.length() - 2));
+    }
+
     while(formulaStream >> value) {
         if (value.front() == '(') {
             if (nestLevel == 0 && andIndex != -1) {
@@ -49,6 +53,7 @@ struct formula * parse(std::string formula) {
                 orIndex = formulaStream.tellg();
                 break;
             }
+            continue;
         }
         isLiteral = true;
 
