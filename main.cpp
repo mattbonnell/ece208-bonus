269d8cfb121704cfbc3c5aa2c95ca5a9108f4802
Matt Bonnell <mdcbonne@edu.uwaterloo.ca>
2019-07-16 22:29:36 -0400

    fe

--- main.cpp
+++ main.cpp
@@ -1,16 +1,18 @@
 #include "parse.h"
 #include "tseitin_replacement.h"
 #include "treeCNF.h"
+#include <fstream>
 
 int main() {
     std::ifstream ifs("test.txt");
     std::string formula_string((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));
     formula * f = parse(formula_string);
     std::list<formula *> * sub_formulas = perform_tseitin_replacement(f);
+    ofstream outfile ("outputFormula.txt");
     for(std::list<formula *>::iterator it = sub_formulas->begin(); it != sub_formulas->end(); ++it) {
         formula * converted = NNF(*it);
         converted = CNF(converted);
-        printTree(converted);
+        printTree(converted, outfile);
     }
     //test point
     delete f;
