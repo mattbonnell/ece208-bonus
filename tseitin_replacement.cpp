cbee047043a7b1b55a902d4939feca5dec37ac32
Matt Bonnell <mdcbonne@edu.uwaterloo.ca>
2019-07-19 20:43:20 -0400

    ah

--- tseitin_replacement.cpp
+++ tseitin_replacement.cpp
@@ -19,7 +19,7 @@ void _perform_tseitin_replacement_recursive(struct formula * f, std::list<struct
     sub->left->value = sub_var;
     sub->right = f;
     sub_formulas->push_back(sub);
-    printTree(sub)
+    printTree(sub);
     f->value = sub_var;
     // if (f->left) {
     //     delete f->left;
